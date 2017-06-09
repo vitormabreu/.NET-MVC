6219392a1d8bd31c73c97f8131d186d3852d861f
Vitor Abreu <vitinabreu@gmail.com>
2017-06-09 16:43:23 -0300

    Create Médico

--- VMA.CadeMeuMedico/VMA.CadeMeuMedico/Controllers/MedicosController.cs
+++ VMA.CadeMeuMedico/VMA.CadeMeuMedico/Controllers/MedicosController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity.Migrations;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -16,6 +17,31 @@ namespace VMA.CadeMeuMedico.Controllers
         {
             var medicos = db.Medicos.Include("Cidades").Include("Especialidades");
             return View(medicos);
+        }
+
+        // GET: Adicionar
+        public ActionResult Adicionar()
+        {
+            ViewBag.Cidades = new SelectList(db.Cidades, "Cidade", "Nome");
+            ViewBag.Especialidades = new SelectList(db.Especialidades, "Especialidade", "Nome");
+            return View();
+        }
+
+        //POST: Adicionar
+        [HttpPost]
+        public ActionResult Adicionar(Medicos medico)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Medicos.AddOrUpdate(medico);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.Cidades = new SelectList(db.Cidades, "Cidade", "Nome", medico.Cidades);
+            ViewBag.Especialidades = new SelectList(db.Especialidades, "Especialidade", "Nome", medico.Especialidades);
+
+            return View(medico);
         }
     }
 }
