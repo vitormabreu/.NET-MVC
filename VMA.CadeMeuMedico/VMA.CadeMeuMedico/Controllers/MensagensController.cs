7e7c9da2291542b6c8438819daa3862313cfc2bc
Vitor Abreu <vitinabreu@gmail.com>
2017-06-09 14:22:08 -0300

    Controllers

---
+++ VMA.CadeMeuMedico/VMA.CadeMeuMedico/Controllers/MensagensController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace VMA.CadeMeuMedico.Controllers
+{
+    public class MensagensController : Controller
+    {
+        // GET: Mensagens
+        public ActionResult BomDia()
+        {
+            return Content("Bom dia, hoje você acordou cedo.");
+        }
+
+        public ActionResult BoaTarde()
+        {
+            return Content("Não durma na hora do trabalho!");
+        }
+
+        public ActionResult BoaNoite()
+        {
+            return Content("Já está na hora de dormir.");
+        }
+    }
+}
