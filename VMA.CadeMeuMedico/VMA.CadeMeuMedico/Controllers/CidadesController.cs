98e2f3e0d52004cff3ef407c39eae1c3867fe29c
Vitor Abreu <vitinabreu@gmail.com>
2017-06-19 11:45:03 -0300

    Cidades scaffold

---
+++ VMA.CadeMeuMedico/VMA.CadeMeuMedico/Controllers/CidadesController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using VMA.CadeMeuMedico.Models;
+
+namespace VMA.CadeMeuMedico.Controllers
+{
+    public class CidadesController : Controller
+    {
+        private CadeMeuMedicoBDEntities db = new CadeMeuMedicoBDEntities();
+
+        // GET: Cidades
+        public ActionResult Index()
+        {
+            return View(db.Cidades.ToList());
+        }
+
+        // GET: Cidades/Details/5
+        public ActionResult Detalhes(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Cidades cidades = db.Cidades.Find(id);
+            if (cidades == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cidades);
+        }
+
+        // GET: Cidades/Create
+        public ActionResult Adicionar()
+        {
+            return View();
+        }
+
+        // POST: Cidades/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Adicionar([Bind(Include = "IDCidade,Nome")] Cidades cidades)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Cidades.Add(cidades);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(cidades);
+        }
+
+        // GET: Cidades/Edit/5
+        public ActionResult Editar(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Cidades cidades = db.Cidades.Find(id);
+            if (cidades == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cidades);
+        }
+
+        // POST: Cidades/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Editar([Bind(Include = "IDCidade,Nome")] Cidades cidades)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(cidades).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(cidades);
+        }
+
+        // GET: Cidades/Delete/5
+        public ActionResult Excluir(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Cidades cidades = db.Cidades.Find(id);
+            if (cidades == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cidades);
+        }
+
+        // POST: Cidades/Delete/5
+        [HttpPost, ActionName("Excluir")]
+        [ValidateAntiForgeryToken]
+        public ActionResult ConfirmaEsclusao(int id)
+        {
+            Cidades cidades = db.Cidades.Find(id);
+            db.Cidades.Remove(cidades);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
