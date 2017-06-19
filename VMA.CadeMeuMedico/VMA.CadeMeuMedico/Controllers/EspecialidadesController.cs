4e4f1662887ba3aeadd3d4cda9e6a104b7b99973
Vitor Abreu <vitinabreu@gmail.com>
2017-06-19 17:31:01 -0300

    Final adjusts in Layout

---
+++ VMA.CadeMeuMedico/VMA.CadeMeuMedico/Controllers/EspecialidadesController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using VMA.CadeMeuMedico.Models;
+
+namespace VMA.CadeMeuMedico.Controllers
+{
+    public class EspecialidadesController : Controller
+    {
+        private CadeMeuMedicoBDEntities db = new CadeMeuMedicoBDEntities();
+
+        // GET: Especialidades
+        public ActionResult Index()
+        {
+            return View(db.Especialidades.ToList());
+        }
+
+        // GET: Especialidades/Details/5
+        public ActionResult Detalhes(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Especialidades especialidade = db.Especialidades.Find(id);
+            if (especialidade == null)
+            {
+                return HttpNotFound();
+            }
+            return View(especialidade);
+        }
+
+        // GET: Especialidades/Create
+        public ActionResult Adicionar()
+        {
+            return View();
+        }
+
+        // POST: Especialidades/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Adicionar([Bind(Include = "IDEspecialidade,Nome")] Especialidades especialidade)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Especialidades.Add(especialidade);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(especialidade);
+        }
+
+        // GET: Especialidades/Edit/5
+        public ActionResult Editar(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Especialidades especialidade = db.Especialidades.Find(id);
+            if (especialidade == null)
+            {
+                return HttpNotFound();
+            }
+            return View(especialidade);
+        }
+
+        // POST: Especialidades/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Editar([Bind(Include = "IDEspecialidade,Nome")] Especialidades especialidade)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(especialidade).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(especialidade);
+        }
+
+        // GET: Especialidades/Delete/5
+        public ActionResult Excluir(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Especialidades especialidade = db.Especialidades.Find(id);
+            if (especialidade == null)
+            {
+                return HttpNotFound();
+            }
+            return View(especialidade);
+        }
+
+        // POST: Especialidades/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult ConfirmaExclusao(int id)
+        {
+            Especialidades especialidade = db.Especialidades.Find(id);
+            db.Especialidades.Remove(especialidade);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
